ea8f32613eded234ecc2020eefbce56e20a6b220
hashqueue <1912315910@qq.com>
2022-08-10 23:49:06 +0800

    perf: 优化登录页面样式.

--- src/views/login/UserLogin.vue
+++ src/views/login/UserLogin.vue
@@ -1,95 +1,121 @@
 <template>
   <div class="base-container">
-    <a-row class="a-row-flex" justify="center" align="middle">
-      <a-card class="login-box">
-        <a-tabs v-model:activeKey="activeKey" centered>
-          <a-tab-pane key="login" tab="登录">
-            <a-form
-              :model="loginForm"
-              name="login"
-              :rules="loginRules"
-              :label-col="{ span: 8 }"
-              :wrapper-col="{ span: 10 }"
-              autocomplete="off"
-              @finish="onLoginFinish"
-              @finishFailed="onLoginFinishFailed"
-            >
-              <a-form-item label="用户名" name="username">
-                <a-input v-model:value="loginForm.username" placeholder="请输入用户名或邮箱" :allow-clear="true">
-                  <template #prefix>
-                    <UserOutlined />
-                  </template>
-                </a-input>
-              </a-form-item>
+    <div class="top">
+      <div class="header">
+        <img alt="logo" class="logo" src="@/assets/candy.svg" />
+        <span class="title">Candy Admin</span>
+      </div>
+      <div class="desc">Ant Design 是西湖区最具影响力的 Web 设计规范</div>
+    </div>
+    <div class="login-box">
+      <a-tabs v-model:activeKey="activeKey" size="large" centered>
+        <a-tab-pane key="login" tab="登录">
+          <a-form
+            :model="loginForm"
+            name="login"
+            :rules="loginRules"
+            :label-col="{ span: 8 }"
+            :wrapper-col="{ span: 10 }"
+            autocomplete="off"
+            @finish="onLoginFinish"
+            @finishFailed="onLoginFinishFailed"
+          >
+            <a-form-item label="用户名" name="username">
+              <a-input
+                v-model:value="loginForm.username"
+                size="large"
+                placeholder="请输入用户名或邮箱"
+                :allow-clear="true"
+              >
+                <template #prefix>
+                  <UserOutlined />
+                </template>
+              </a-input>
+            </a-form-item>
 
-              <a-form-item label="密码" name="password">
-                <a-input-password v-model:value="loginForm.password" placeholder="请输入密码" :allow-clear="true">
-                  <template #prefix>
-                    <LockOutlined />
-                  </template>
-                </a-input-password>
-              </a-form-item>
+            <a-form-item label="密码" name="password">
+              <a-input-password
+                v-model:value="loginForm.password"
+                size="large"
+                placeholder="请输入密码"
+                :allow-clear="true"
+              >
+                <template #prefix>
+                  <LockOutlined />
+                </template>
+              </a-input-password>
+            </a-form-item>
 
-              <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
-                <a-button type="primary" html-type="submit">登录</a-button>
-              </a-form-item>
-            </a-form>
-          </a-tab-pane>
-          <a-tab-pane key="register" tab="注册">
-            <a-form
-              :model="registerForm"
-              name="register"
-              :rules="registerRules"
-              :label-col="{ span: 8 }"
-              :wrapper-col="{ span: 10 }"
-              autocomplete="off"
-              @finish="onRegisterFinish"
-              @finishFailed="onRegisterFinishFailed"
-            >
-              <a-form-item label="用户名" name="username">
-                <a-input v-model:value="registerForm.username" placeholder="请输入用户名" :allow-clear="true">
-                  <template #prefix>
-                    <UserOutlined />
-                  </template>
-                </a-input>
-              </a-form-item>
+            <a-form-item :wrapper-col="{ offset: 8, span: 10 }">
+              <a-button type="primary" size="large" class="login-button" html-type="submit">登录</a-button>
+            </a-form-item>
+          </a-form>
+        </a-tab-pane>
+        <a-tab-pane key="register" tab="注册">
+          <a-form
+            :model="registerForm"
+            name="register"
+            :rules="registerRules"
+            :label-col="{ span: 8 }"
+            :wrapper-col="{ span: 10 }"
+            autocomplete="off"
+            @finish="onRegisterFinish"
+            @finishFailed="onRegisterFinishFailed"
+          >
+            <a-form-item label="用户名" name="username">
+              <a-input
+                v-model:value="registerForm.username"
+                size="large"
+                placeholder="请输入用户名"
+                :allow-clear="true"
+              >
+                <template #prefix>
+                  <UserOutlined />
+                </template>
+              </a-input>
+            </a-form-item>
 
-              <a-form-item label="邮箱" name="email">
-                <a-input v-model:value="registerForm.email" placeholder="请输入邮箱" :allow-clear="true">
-                  <template #prefix>
-                    <MailOutlined />
-                  </template>
-                </a-input>
-              </a-form-item>
+            <a-form-item label="邮箱" name="email">
+              <a-input v-model:value="registerForm.email" size="large" placeholder="请输入邮箱" :allow-clear="true">
+                <template #prefix>
+                  <MailOutlined />
+                </template>
+              </a-input>
+            </a-form-item>
 
-              <a-form-item label="密码" name="password">
-                <a-input-password v-model:value="registerForm.password" placeholder="请输入密码" :allow-clear="true">
-                  <template #prefix>
-                    <LockOutlined />
-                  </template>
-                </a-input-password>
-              </a-form-item>
+            <a-form-item label="密码" name="password">
+              <a-input-password
+                v-model:value="registerForm.password"
+                size="large"
+                placeholder="请输入密码"
+                :allow-clear="true"
+              >
+                <template #prefix>
+                  <LockOutlined />
+                </template>
+              </a-input-password>
+            </a-form-item>
 
-              <a-form-item label="确认密码" name="password_confirm">
-                <a-input-password
-                  v-model:value="registerForm.password_confirm"
-                  placeholder="请输入确认密码"
-                  :allow-clear="true"
-                >
-                  <template #prefix>
-                    <LockOutlined />
-                  </template>
-                </a-input-password>
-              </a-form-item>
+            <a-form-item label="确认密码" name="password_confirm">
+              <a-input-password
+                v-model:value="registerForm.password_confirm"
+                size="large"
+                placeholder="请再次输入密码"
+                :allow-clear="true"
+              >
+                <template #prefix>
+                  <LockOutlined />
+                </template>
+              </a-input-password>
+            </a-form-item>
 
-              <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
-                <a-button type="primary" html-type="submit">注册</a-button>
-              </a-form-item>
-            </a-form>
-          </a-tab-pane>
-        </a-tabs>
-      </a-card>
-    </a-row>
+            <a-form-item :wrapper-col="{ offset: 8, span: 10 }">
+              <a-button type="primary" size="large" class="login-button" html-type="submit">注册</a-button>
+            </a-form-item>
+          </a-form>
+        </a-tab-pane>
+      </a-tabs>
+    </div>
   </div>
 </template>
 
@@ -146,15 +172,50 @@ const onRegisterFinishFailed = (errorInfo) => {
 
 <style scoped lang="less">
 .base-container {
-  width: 100vw;
+  display: flex;
+  // 元素垂直显示
+  flex-direction: column;
   height: 100vh;
-  background: #2c5364 url('src/assets/background.svg');
-  .a-row-flex {
-    height: 100%;
+  overflow: auto;
+  background: #f0f2f5 url('src/assets/background.svg') no-repeat;
+  background-position-x: center;
+  background-position-y: 110px;
+  background-size: 100%;
+  @media (min-width: 768px) {
+    padding: 112px 0 24px;
+  }
+  .top {
+    text-align: center;
+    .header {
+      height: 44px;
+      line-height: 44px;
+      .logo {
+        height: 44px;
+        vertical-align: top;
+        margin-right: 16px;
+      }
+      .title {
+        font-size: 33px;
+        color: fade(#000, 85%);
+        font-family: 'Myriad Pro', 'Helvetica Neue', Arial, Helvetica, sans-serif;
+        font-weight: 600;
+        position: relative;
+        top: 2px;
+      }
+    }
+    .desc {
+      font-size: 14px;
+      color: fade(#000, 45%);
+      margin-top: 12px;
+      margin-bottom: 40px;
+    }
   }
   .login-box {
     width: 800px;
-    height: 500px;
+    margin: 0 auto;
+    .login-button {
+      width: 100%;
+    }
   }
 }
 </style>
