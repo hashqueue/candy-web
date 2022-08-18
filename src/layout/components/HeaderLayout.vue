d3fadb014ba1dd77858222fc47c4f1d9aeecbd69
hashqueue <1912315910@qq.com>
2022-08-19 01:21:04 +0800

    feat: add screenfull 网页全屏功能.

--- src/layout/components/HeaderLayout.vue
+++ src/layout/components/HeaderLayout.vue
@@ -1,6 +1,6 @@
 <template>
-  <a-row justify="space-between">
-    <a-col :span="22">
+  <a-row type="flex" justify="space-between">
+    <a-col :span="20">
       <a-row>
         <a-col><img class="logo-img" src="@/assets/candy.svg" alt="logo" /></a-col>
         <a-col><h1 class="logo-title">Candy</h1></a-col>
@@ -9,8 +9,11 @@
         </a-col>
       </a-row>
     </a-col>
-    <a-col :span="2">
-      <a-row justify="end">
+    <a-col :span="4">
+      <a-row type="flex" justify="end">
+        <a-col>
+          <screenfull-view />
+        </a-col>
         <a-col>
           <a-popover :title="`Hello ${userInfo.username}!`">
             <template #content>
@@ -33,6 +36,7 @@ import { userStore } from '@/stores/user'
 import { removeAllItem } from '@/utils/storage'
 import MenuLayout from './menu/MenuLayout.vue'
 import BreadcrumbLayout from './BreadcrumbLayout.vue'
+import ScreenfullView from '@/components/ScreenfullView.vue'
 
 const userInfoStore = userStore()
 const userInfo = computed(() => userInfoStore.getUserInfo)
@@ -46,6 +50,7 @@ const logOut = () => {
 <style scoped>
 .avatar {
   margin-left: 15px;
+  cursor: pointer;
 }
 
 .logo-img {
